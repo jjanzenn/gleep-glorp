66d7c0fd889b8f0050e63517992defc3e03c5f68
Zoey Kitt <zoey.kitt@outlook.com>
2025-11-29 17:08:47 -0600

    Hearts now explode

--- border/border.gd
+++ border/border.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var lives: int = 4
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -15,3 +17,13 @@ func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
 		print("win :))")
 	else:
 		print("fail :((")
+		match lives:
+			1:
+				$"Lives/Life 1/AnimationPlayer".play("explod")
+			2:
+				$"Lives/Life 2/AnimationPlayer".play("explod")
+			3:
+				$"Lives/Life 3/AnimationPlayer".play("explod")
+			4:
+				$"Lives/Life 4/AnimationPlayer".play("explod")
+		lives -= 1
