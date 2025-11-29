558ec87146fd2217d1f7b7cf1fd945c659baf6e5
Zoey Kitt <zoey.kitt@outlook.com>
2025-11-29 17:56:13 -0600

    Rudimentary between-game stuff

--- border/border.gd
+++ border/border.gd
@@ -13,18 +13,27 @@ func _process(_delta: float) -> void:
 	pass
 
 
+func _on_timer_animation_finished(_anim_name: StringName) -> void:
+	if _anim_name == "timer":
+		$Microgame/root.queue_free()
+		
+		if $Microgame/root.won:
+			score += 1
+			$Score.text = str(score)
+		else:
+			match lives:
+				1:
+					$"Lives/Life 1/AnimationPlayer".play("explod")
+				2:
+					$"Lives/Life 2/AnimationPlayer".play("explod")
+				3:
+					$"Lives/Life 3/AnimationPlayer".play("explod")
+				4:
+					$"Lives/Life 4/AnimationPlayer".play("explod")
+			lives -= 1
+			
+		$AnimationPlayer.play("win")
+
+
 func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
-	if $Microgame/root.won:
-		score += 1
-		$Score.text = str(score)
-	else:
-		match lives:
-			1:
-				$"Lives/Life 1/AnimationPlayer".play("explod")
-			2:
-				$"Lives/Life 2/AnimationPlayer".play("explod")
-			3:
-				$"Lives/Life 3/AnimationPlayer".play("explod")
-			4:
-				$"Lives/Life 4/AnimationPlayer".play("explod")
-		lives -= 1
+	print("waiter waiter one more game please")
