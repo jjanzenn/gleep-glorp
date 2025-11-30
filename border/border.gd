25ec8af70f1543b8a3117c5e3ddf3692f0258cc5
Zoey Kitt <zoey.kitt@outlook.com>
2025-11-29 18:16:36 -0600

    Stop spawning new microgames after losing all lives

--- border/border.gd
+++ border/border.gd
@@ -18,6 +18,7 @@ func _on_timer_animation_finished(_anim_name: StringName) -> void:
 		$Microgame/root.queue_free()
 		
 		if $Microgame/root.won:
+			$AnimationPlayer.play("win")
 			score += 1
 			$Score.text = str(score)
 		else:
@@ -30,13 +31,17 @@ func _on_timer_animation_finished(_anim_name: StringName) -> void:
 					$"Lives/Life 3/AnimationPlayer".play("explod")
 				4:
 					$"Lives/Life 4/AnimationPlayer".play("explod")
+			if lives > 1:
+				$AnimationPlayer.play("lose")
+			else:
+				$AnimationPlayer.play("game_over")
 			lives -= 1
 			
-		$AnimationPlayer.play("win")
 
 
-func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	print("waiter waiter one more game please")
 	
-	$Microgame.add_child(preload("res://microgames/fishing/root.tscn").instantiate())
-	$timer/AnimationPlayer.play("timer")
+	if anim_name == "win" or anim_name == "lose":
+		$Microgame.add_child(preload("res://microgames/fishing/root.tscn").instantiate())
+		$timer/AnimationPlayer.play("timer")
